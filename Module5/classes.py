f88b6ce3f7d4d42f094db086fa42ed76b1721cd9
Stevenson Yan <stevensoncodes@gmail.com>
2025-08-15 05:17:46 -0700

    updated module5

--- Module5/classes.py
+++ Module5/classes.py
@@ -14,6 +14,17 @@ class Tester(Employee):
         print('Tests are done.')
 
 class Developer(Employee):
+    def __init__(self, name, age, salary, framework):
+        super().__init__(name, age, salary)
+        self.framework = framework
+
     def increase_salary(self, percent, bonus=0):
         super().increase_salary(percent)
         self.salary += bonus
+
+employee1 = Tester("Joseph", 28, 2000)
+employee2 = Developer("Linda", 22, 4000, "Flask")
+
+employee2.increase_salary(20, 30)
+print(employee2.name, employee2.framework)
+
